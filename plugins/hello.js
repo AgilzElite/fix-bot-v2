2a5b4918b3f7112b90f1dfa5599417de8d13da72
AgilzElite <113176792+AgilzElite@users.noreply.github.com>
2022-09-23 21:06:40 +0700

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "jasa jadibot/buy sc ke https://wa.me/6285743137723",
+    mediaUrl: "https://telegra.ph/file/90a931648de597820bc08.jpg",
     mediaType: 2,
     description: "jasa jadibot/buy sc ke https://wa.me/6285743137723", 
     title: 'anjay-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
