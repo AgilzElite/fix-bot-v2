865d647f6a3c986e9ec1d94f288d3ca169e54ec9
AgilzElite <113176792+AgilzElite@users.noreply.github.com>
2022-09-23 13:35:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 /*============== STAFF ==============*/
 global.owner = [
   ['6285857537269', 'Gilzz Hosting', true],
-  ['-', 'isi_kalau_ada', true],
+  ['6282180426880', 'Toder Ketche', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
